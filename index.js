d383f55f8f787f0c5eb7433b6f30c76602eb232a
tinyfgt <7epicf@gmail.com>
2020-07-17 14:26:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -257,7 +257,7 @@ else message.channel.send (argsr)
              if (reaction.emoji.name === "✔️"){
                 
                 viesti.edit(argsr)
-                message.channel.send(`${argsr} ${cmd.author.username}`)
+                message.channel.send(`${argsr} ${message.author.username}`)
                 return;
                 
              }})
