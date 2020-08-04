8da163249328a9a82a33cf71b7cfa3e9599f6687
tinyfgt <7epicf@gmail.com>
2020-08-04 03:07:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -91,6 +91,7 @@ bot.on("message", async message => {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
+    if (message.author === bot) return;
 
 
 
